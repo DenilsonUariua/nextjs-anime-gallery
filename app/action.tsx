2270ee82decab9e6b1afaeb271836c174b61752a
Denilson Uariua <denuariua@gmail.com>
2023-12-04 14:26:25 +0200

    completed loading

---
+++ app/action.tsx
@@ -0,0 +1,14 @@
+"use server";
+
+import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
+
+export const fetchAnime = async (page: number, limit: number) => {
+  const response = await fetch(
+    `https://shikimori.one/api/animes?page=${page}&limit=${limit}&order=popularity`
+  );
+  const data = await response.json();
+
+  return data.map((item: AnimeProp, index: number) => (
+    <AnimeCard key={item.id} anime={item} index={index} />
+  ));
+};
